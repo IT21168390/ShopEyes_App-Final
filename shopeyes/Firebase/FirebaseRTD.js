235ebb3f9caf268b6258a881da01996bb10b5689
Hashan Madhuwantha <99306792+IT21168390@users.noreply.github.com>
2025-01-30 22:36:56 +0530

    Update FirebaseRTD.js

--- shopeyes/Firebase/FirebaseRTD.js
+++ shopeyes/Firebase/FirebaseRTD.js
@@ -6,22 +6,8 @@ import { getDatabase, ref, push, get } from 'firebase/database';
 
 //  web app's Firebase configuration
 /*const firebaseConfig = {
-    apiKey: "AIzaSyD8-3-sSKZNhmq_aAj4WwBdogHdYQAI4v4",
-    authDomain: "shopeyes-rn-project-1.firebaseapp.com",
-    databaseURL: "https://shopeyes-rn-project-1-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "shopeyes-rn-project-1",
-    storageBucket: "shopeyes-rn-project-1.appspot.com",
-    messagingSenderId: "54881385649",
-    appId: "1:54881385649:web:bc125a7453a9e34598f690"
 };*/
 const firebaseConfig = {
-    apiKey: "AIzaSyBgwJMpX4LUSEHDs0C2O0oL2XjBQY_pqQU",
-    authDomain: "shop-e0971.firebaseapp.com",
-    databaseURL: "https://shop-e0971-default-rtdb.firebaseio.com",
-    projectId: "shop-e0971",
-    storageBucket: "shop-e0971.appspot.com",
-    messagingSenderId: "204728642679",
-    appId: "1:204728642679:web:70fbe2cd8baf88dbeaaa72"
 };
 
 // Initialize Firebase
