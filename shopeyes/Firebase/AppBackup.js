cc76a3b57cd4571c4f3c9ffdf7c7267cad4b14d6
Hashan Madhuwantha <99306792+IT21168390@users.noreply.github.com>
2025-01-30 22:37:27 +0530

    Update AppBackup.js

--- shopeyes/Firebase/AppBackup.js
+++ shopeyes/Firebase/AppBackup.js
@@ -19,13 +19,6 @@ import { getDatabase, ref, push, get } from 'firebase/database';
 
 //  web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyD8-3-sSKZNhmq_aAj4WwBdogHdYQAI4v4",
-  authDomain: "shopeyes-rn-project-1.firebaseapp.com",
-  databaseURL: "https://shopeyes-rn-project-1-default-rtdb.asia-southeast1.firebasedatabase.app",
-  projectId: "shopeyes-rn-project-1",
-  storageBucket: "shopeyes-rn-project-1.appspot.com",
-  messagingSenderId: "54881385649",
-  appId: "1:54881385649:web:bc125a7453a9e34598f690"
 };
 
 // Initialize Firebase
